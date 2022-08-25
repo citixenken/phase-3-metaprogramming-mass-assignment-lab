80c4de3e95f27b3f22f8478d998e597e548b9b6e
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-08-25 22:27:22 +0300

    COMPLETED: mass assignment lab

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,28 @@
 class Person
-  # your code here
+  def initialize(args)
+    # args.each_key do |k, v| 
+    #   self.class.attr_accessor(k)
+    #   self.send("#{k}=", args[k])
+    # end
+
+    args.each do |k, v| 
+      self.class.attr_accessor(k)
+      self.send("#{k}=", v)
+    end
+  end
 end
+
+# confirming output
+#==================
+# bob_attributes = { name: "Bob", hair_color: "Brown" }
+
+# bob = Person.new(bob_attributes)
+# puts bob.name       # => "Bob"
+# puts bob.hair_color # => "Brown"
+
+# susan_attributes = { name: "Susan", height: "5'11\"", eye_color: "Green" }
+
+# susan = Person.new(susan_attributes)
+# puts susan.name      # => "Susan"
+# puts susan.height    # => "5'11""
+# puts susan.eye_color # => "Green"
